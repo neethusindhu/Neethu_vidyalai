6a644da5c63fd2c0a03c02339766f788a8fe7249
neethusindhu <neethus106@gmail.com>
2024-08-13 16:07:14 +0530

    Add Files

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app
     server.listen(3000, err => {
       if (err) throw err;
       console.log('> Ready on http://localhost:3000');
-      console.log("connected to server");
+      console.log("connected to server...");
     });
   })
   .catch(ex => {
